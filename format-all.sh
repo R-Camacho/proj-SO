32b8c7d357f8ba77312329e3aa7c096a2c898672
R-Camacho <rodrigompcamacho@gmail.com>
2024-11-13 22:04:51 +0000

    Remove obsolete formatting script and add run script for build and execution

--- format-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd src
-find . -regex '.*\.\(cpp\|hpp\|cu\|cuh\|c\|h\)' -exec clang-format -style=file -i {} \;
-cd ..
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Use 'set -e' to exit the script if any command fails
+set -e
+
+# Function to handle cleanup
+cleanup() {
+    echo "Cleaning up..."
+    make clean
+}
+
+# Trap INT (CTRL-C) and TERM signals to call the cleanup function
+trap cleanup INT TERM
+
+# Move to the src directory and execute the commands
+cd src
+make format
+
+# Execute make run
+make
+./kvs
+cd ..
